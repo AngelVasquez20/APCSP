5f60b31751de90ac6b72ce725b9a37dc99156e19
AngelVasquez20 <42391751+AngelVasquez20@users.noreply.github.com>
2019-09-06 15:42:35 -0700

    9/6/19

--- Palindrome Checker.py
+++ Palindrome Checker.py
@@ -1,1 +1,14 @@
-import string
+def palindrome():
+    palindrome_word = input("Enter a word for a palindrome: ")
+    palindrome_word.lower()
+    palindrome_word.replace(" ", "")
+    if palindrome_word == palindrome_word[::-1]:
+        print("This is a palindrome")
+        return True
+
+    else:
+        print("This is not a palindrome")
+        return False
+
+
+palindrome()
