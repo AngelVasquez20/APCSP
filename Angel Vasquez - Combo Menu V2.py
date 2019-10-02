5bc1a071b0ffc526b5e1f9ce55f87bcd63cd4488
AngelVasquez20 <42391751+AngelVasquez20@users.noreply.github.com>
2019-10-02 15:42:10 -0700

    10/2/19

--- Angel Vasquez - Combo Menu V2.py
+++ Angel Vasquez - Combo Menu V2.py
@@ -1,29 +1,42 @@
+import sys
 total = 0
 
-sandwich_list = ["Chicken", "Tofu", "Beef"]
+sandwich_list = ["chicken", "tofu", "beef"]
 sizes = ["small", "medium", "large"]
+answers = []
+yes_cost = []
 
 
 def sandwich():
-    sandq = input("Would you like a sandwich? ")
+    sand_question = input("Would you like a sandwich (Type in yes or no)? ")
     sandqprice = 0
-    if sandq == "yes".lower():
-        ask = input("What kind of sandwich would you like 1 for chicken 2 for tofu 3 for beef:")
-        if ask == "1":
-            print("You ordered a Chicken sandwich")
-            sandqprice += float(5.25)
-        if ask == "2":
-            sandqprice += float(5.75)
-            print("You ordered a Tofu sandwich")
-        if ask == "3":
-            sandqprice += float(6.25)
-            print("You ordered a Beef sandwich")
+    if sand_question == "no":
+        answers.append(0)
+        yes_cost.append(int(0))
         return sandqprice
-    if sandq == "no".lower():
-        return False
-    while sandq is not "yes" or sandq is not "no":
-        print("I didn't get that")
-        sandq = input("Would you like a sandwich? ")
+    if sand_question == "q":
+        sys.exit(0)
+    if sand_question == "yes":
+        ask = input("What kind of sandwich would you like chicken, tofu, or beef:").lower()
+        if ask in sandwich_list:
+            if ask == sandwich_list[0]:
+                sandqprice += 5.25
+                print("You ordered a Chicken sandwich")
+                answers.append(ask)
+                yes_cost.append(sandqprice)
+            elif ask == sandwich_list[1]:
+                sandqprice += 5.75
+                print("You ordered a Tofu sandwich")
+                answers.append(ask)
+                yes_cost.append(sandqprice)
+            elif ask == sandwich_list[2]:
+                sandqprice += 6.25
+                print("You have ordered a Beef sandwich")
+                answers.append(ask)
+                yes_cost.append(sandqprice)
+        return sandqprice
+    while sand_question is not "yes" or sand_question is not "no":
+        sand_question = input("Invalid information. Would you like a sandwich (yes or no)")
 
 
 sandwich()
@@ -105,10 +118,10 @@ def ketchup():
             print("Your total will be $%.2f" % packet_cost)
             return packet_cost
 
-
 ketchup()
 
-total += sandwich()
-total += beverage()
-total += french_fries()
-total += ketchup()
+
+# total += sandwich()
+# total += beverage()
+# total += french_fries()
+# total += ketchup()
